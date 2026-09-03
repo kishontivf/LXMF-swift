93e3d0c060ddf2373b68d3af8e712aea4f6ca426
Andras Olah <andras.olah@kishonti.net>
2026-09-03 10:01:15 +0200

    Version bump

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/kishontivf/reticulum-swift.git", from: "0.4.4"),
-        // .package(path: "../reticulum-swift"),
+        .package(url: "https://github.com/kishontivf/reticulum-swift.git", from: "0.5.0"),
+//        .package(path: "../reticulum-swift"),
         .package(url: "https://github.com/groue/GRDB.swift.git", from: "6.0.0"),
         // Cap below 4.9.0: SWCompression 4.9.0 raised its floor to macOS 14 / iOS 17,
         // above this library's macOS 13 / iOS 16 (which matches the sibling ports
